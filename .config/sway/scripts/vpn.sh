e61c49d1a5fa391c79be4a9c4b6aa2e06319eb05
Anatoliy Khomyakov <a.khomyakov@pm.me>
2020-03-23 11:27:50 +0700

    Refactor vpn script

--- .config/sway/scripts/vpn.sh
+++ .config/sway/scripts/vpn.sh
@@ -4,12 +4,14 @@
 
 toggle_vpn()
 {
-    vpn_status=$(protonvpn status | awk '/Status/{print $2}')
+    if [ "$VPN_PROVIDER" = 'protonvpn' ]; then
+        vpn_status=$(protonvpn status | awk '/Status/{print $2}')
 
-    if [ "$vpn_status" = "Connected" ]; then
-        notify-send 'Disconnecting VPN...' && sudo protonvpn disconnect | while read result; do notify-send "$result"; done
-    else
-        notify-send 'Connecting VPN...' && sudo protonvpn connect -f | while read result; do notify-send "$result"; done
+        if [ "$vpn_status" = "Connected" ]; then
+            notify-send 'Disconnecting VPN...' & sudo protonvpn disconnect | while read result; do notify-send "$result"; done
+        else
+            notify-send 'Connecting VPN...' & sudo protonvpn connect -f | while read result; do notify-send "$result"; done
+        fi
     fi
 }
 
